927eaf4e992c0773c3cadb74fa9e977830cbc33f
Michel Hamelink <info@michelhamelink.nl>
2024-09-26 11:30:35 +0200

    initial commit

---
+++ src/Home.tsx
@@ -0,0 +1,14 @@
+import './index.scss';
+
+function App() {
+  return (
+      <div className="">
+          <h1 className="text-3xl font-bold underline">
+              Hello world!
+          </h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, error?</p>
+      </div>
+  );
+}
+
+export default App;
