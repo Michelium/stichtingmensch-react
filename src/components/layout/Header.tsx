d867b95c5883fc06b072d4bf8667b708aefb7512
Michel Hamelink <info@michelhamelink.nl>
2024-09-26 12:34:27 +0200

    feat: header logo height max height

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
             <header className="flex justify-between container h-[4rem] mx-auto px-4 lg:px-0 pt-5">
                 <div className='flex items-center'>
                     <a className="" href="">
-                        <img src="/assets/images/logo.png" alt="logo" className="w-3/5 md:p-3 y-auto object-contain" />
+                        <img src="/assets/images/logo.png" alt="logo" className="w-3/5 lg:p-3 y-auto object-contain xl:max-h-24" />
                     </a>
                 </div>
                 <nav className="hidden md:flex my-auto md:gap-x-5 xl:gap-x-12">
