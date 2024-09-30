5beb446d1a81117b7637f6bf75f10b23d9cda8e2
Michel Hamelink <info@michelhamelink.nl>
2024-09-30 11:31:14 +0200

    feat: removed /build from gitignore, shared hosting :(

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,50 +2,32 @@ set :application, 'stichtingmensch-react'
 set :repo_url, 'git@github.com:Michelium/stichtingmensch-react.git'
 set :tmp_dir, '/home/u114524p110947/tmp'
 
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :repo_tree, 'build'
 
-# Keep the last 5 releases
 set :keep_releases, 5
 
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+
 namespace :deploy do
-  desc 'Install correct Node.js version from .nvmrc'
-  task :load_nvm do
-    on roles(:app) do
-      within release_path do
-        execute "cd #{release_path} && source $HOME/.bashrc && nvm install"
-      end
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+#         execute "cd #{release_path} && npm install"
+#         execute "cd #{release_path} && npm run build"
     end
   end
 
-  desc 'Install node modules'
-  task :npm_install do
-    on roles(:app) do
-      within release_path do
-        execute "cd #{release_path} && source $HOME/.bashrc && nvm use && npm install"
-      end
+  after :updated, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+      #
     end
   end
 
-  desc 'Build the React app'
-  task :npm_run_build do
-    on roles(:app) do
-      within release_path do
-        execute "cd #{release_path} && source $HOME/.bashrc && nvm use && npm run build"
-      end
-    end
-  end
-
-#   desc 'Empty tmp directory'
-#   task :cap_empty_tmp do
-#     on roles(:app) do
-#       within release_path do
-#         execute "rm -rf #{fetch(:tmp_dir)}/*"
-#       end
-#     end
-#   end
-
-  before :npm_install, :load_nvm
-  after :updated, 'deploy:npm_install'
-  after :npm_install, 'deploy:npm_run_build'
-  after :npm_run_build, 'deploy:cap_empty_tmp'
 end
