c078f1384fce6a2abda21769b943a848b8fdd672
Michel Hamelink <info@michelhamelink.nl>
2026-04-06 14:15:06 +0200

    feat: migrate project to Vite with React and TypeScript and update react and dependencies

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './Home';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  React.createElement(React.StrictMode, null, React.createElement(App))
+);
+
+
